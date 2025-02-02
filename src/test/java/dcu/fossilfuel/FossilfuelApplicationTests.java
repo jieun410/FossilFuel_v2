2db6bcd2cf50de6ff16c9487153eb2a9adb1b6c7
adorahelen <adorahelenmin@gmail.com>
2025-02-02 16:40:13 +0900

    [debug] aws ec2 doesnt compile

---
+++ src/test/java/dcu/fossilfuel/FossilfuelApplicationTests.java
@@ -0,0 +1,13 @@
+package dcu.fossilfuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FossilfuelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
